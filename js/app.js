decc3e0f9a4ab2a5dd6a384286c1af5f2e8552cd
Irman Ahmetovic <IAhmetovic@kaplaninc.com>
2018-02-26 22:49:14 -0500

    Clearing out unused files

--- js/app.js
+++ js/app.js
@@ -2,16 +2,16 @@ $(document).ready(function(){
     var lsWrapper = $(".ls-wrapper");
     
     $("#ls-vidToggle").click(function(){
-        if($("#ls-vidDiv").hasClass('col-md-8')){
+        if($("#ls-vid-wrapper").hasClass('col-md-8')){
             $("#ls-video").removeAttr("style");
             $(this).text('Shrink Video');
-            $("#ls-vidDiv").removeClass('col-md-8');
-            $("#ls-vidDiv").addClass('col-md-12');    
+            $("#ls-vid-wrapper").removeClass('col-md-8');
+            $("#ls-vid-wrapper").addClass('col-md-12');    
             $("#ls-video").toggleClass('xl');
             $("#ls-chat").toggleClass('xl');
             
             
-            var chat = document.getElementById('ls-chatDiv');
+            var chat = document.getElementById('ls-chat-wrapper');
             lsWrapper.append(chat);
             chat.parentNode.removeChild(elem);
             
@@ -19,12 +19,12 @@ $(document).ready(function(){
         }else {
             $("#ls-video").removeAttr("style");
             $(this).text('Expand Video');
-            $("#ls-vidDiv").removeClass('col-md-12');
-            $("#ls-vidDiv").addClass('col-md-8');
+            $("#ls-vid-wrapper").removeClass('col-md-12');
+            $("#ls-vid-wrapper").addClass('col-md-8');
             $("#ls-video").toggleClass('xl');
             $("#ls-chat").toggleClass('xl');
             
-            var poll = document.getElementById('ls-pollDiv');
+            var poll = document.getElementById('ls-poll-wrapper');
             lsWrapper.append(poll);
             chat.parentNode.removeChild(poll);
         }
